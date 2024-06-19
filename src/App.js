fe7fcf3f3316df01f941c06c5ca90422dc23ba1e
matthewadeboyejr <matthewconsult@gmail.com>
2024-06-19 13:57:52 +0100

    completed the about service page remaining to make it reponsive for web

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
 import Resetpass from "./pages/Resetpass";
+import Aboutservice from "./pages/Aboutservice";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       <Route path="signup" element={<Signup />} />
       <Route path="dashboard" element={<Dashboard />} />
       <Route path="reset-password" element={<Resetpass />} />
+      <Route path="about-service" element={<Aboutservice />} />
     </Routes>
   );
 };
