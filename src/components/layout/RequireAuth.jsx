f9fb25d2f709ec53f32a9834a63bbdbb5d76136c
matthewadeboyejr <matthewconsult@gmail.com>
2024-09-09 03:24:23 +0100

    fixing vercel build error

--- src/components/layout/RequireAuth.jsx
+++ src/components/layout/RequireAuth.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { useAuthContext } from "../hooks/useAuthContext";
 import { Navigate, Outlet, useLocation } from "react-router-dom";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 const RequireAuth = () => {
   const { isAuthenticated } = useAuthContext();
