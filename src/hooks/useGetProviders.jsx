2032c1881d15d0fed1ce22376558bf1668b7acd8
matthewadeboyejr <matthewconsult@gmail.com>
2024-11-02 01:14:55 +0100

    changes

--- src/hooks/useGetProviders.jsx
+++ src/hooks/useGetProviders.jsx
@@ -2,14 +2,8 @@ import { useEffect, useState } from "react";
 import axiosInstance from "../api/axios";
 
 const useGetProviders = (endPoint, id, postalCode, budget, description) => {
-  /*  const { availableService } = useArtisanContext();
-  const { postalCode } = useAddressContext(); */
   const [exporedService, setExporedService] = useState([]);
   const [loading, setLoading] = useState(false);
-
-  /* const id = availableService?.job_detail?.job_category;
-  const budget = availableService?.job_detail?.budget_range;
-  const description = availableService?.job_detail?.service_description; */
 
   useEffect(() => {
     if (id && postalCode) {
@@ -22,13 +16,11 @@ const useGetProviders = (endPoint, id, postalCode, budget, description) => {
     const url = `${endPoint}?category_id=${id}&postcode=${postalCode}`;
     const data = { budget, description };
 
-    console.log(url, "url url url url");
     try {
       const response = await axiosInstance.post(url, data);
 
       if (response) {
         setExporedService(response?.data?.data.artisans);
-        console.log(response?.data?.data, "from explore provider");
       }
       setLoading(false);
     } catch (error) {
