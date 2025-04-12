7afb3d24907edd5b09cc30b5668dc1e309747ca5
david alaka <alakadavid05@gmail.com>
2025-04-12 05:40:50 +0000

    Edit deploy-to-ec2.sh

--- deploy-to-ec2.sh
+++ deploy-to-ec2.sh
@@ -7,7 +7,7 @@ DOCKER_PULL_COMMAND="docker pull $APP_CONTAINER_IMAGE"
 
 CONTAINER_STOP_COMMAND="if [ \$(docker ps -q -f name=$APP_CONTAINER_NAME) ]; then docker stop $APP_CONTAINER_NAME && docker rm $APP_CONTAINER_NAME; if [ $? -eq 0 ]; then echo 'Container stopped successfully.'; else echo 'Failed to stop the container.'; exit 1; fi; else echo 'Container $APP_CONTAINER_NAME is not running. Skipping stop command.'; fi"
 
-CONTAINER_START_COMMAND="docker run -d --restart unless-stopped -p 80:3001 --name $APP_CONTAINER_NAME $APP_CONTAINER_IMAGE"
+CONTAINER_START_COMMAND="docker run -d --restart unless-stopped -p 80:3000 --name $APP_CONTAINER_NAME $APP_CONTAINER_IMAGE"
 
 COMMAND_ID=$(aws ssm send-command \
     --document-name "AWS-RunShellScript" \
